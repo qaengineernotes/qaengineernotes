e88efc264485a6e9f32e7f0b5d3878002a2d252c
qaengineernotes <qaengineernotes@gmail.com>
2025-11-20 13:09:44 +0530

    feat: implement extensionless URLs
    - Remove .html extensions from all internal links in HTML files
    
    - Remove .html extensions from paths in blogs.json
    
    - Update server.py to support extensionless URLs for local development
    
    - Update common.js to generate extensionless links for search results

--- server.py
+++ server.py
@@ -1,9 +1,21 @@
 import http.server
 import socketserver
+import os
+import socketserver
 
 PORT = 8080
 
 class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # Check if the path has an extension
+        if '.' not in self.path:
+            # Try to find a corresponding .html file
+            html_path = self.path.rstrip('/') + '.html'
+            if os.path.exists(os.path.join(os.getcwd(), html_path.lstrip('/'))):
+                self.path = html_path
+        
+        super().do_GET()
+
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         super().end_headers()
